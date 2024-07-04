ada4617cdc21b60aeb5f6957734fb9badafbb777
Alper Balbay <itsc0508@gmail.com>
2024-07-04 17:01:17 +0300

    added demo banner image

--- src/config.ts
+++ src/config.ts
@@ -11,11 +11,11 @@ export const siteConfig: SiteConfig = {
   subtitle: 'Portfolio',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 280,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
+    enable: true,
     src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
