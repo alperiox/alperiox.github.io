d931cf3ae17345fd1d0f41f19e901ca4524b961e
Alper Balbay <itsc0508@gmail.com>
2024-07-04 17:45:08 +0300

    changed the title

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'alperiox.dev',
-  subtitle: 'Portfolio',
+  title: 'alperiox',
+  subtitle: 'blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 280,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
